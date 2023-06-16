b9c2525025c4587088cbc30e0a373c7738c3228a
Blackranger07 <44442074+blackranger07@users.noreply.github.com>
2023-06-16 11:59:49 -0400

    Modified automatedsetup function
    Script now works.

--- Automated-System-Update-Installer.sh
+++ Automated-System-Update-Installer.sh
@@ -11,42 +11,39 @@ automationsetup () {
 
 FILEDIR="/etc/systemd/system"
 
-echo "Creating files Ubuntu-Packages-Update.service and Ubuntu-Packages-Update.timer in /etc/systemd/system"
-sleep 2
 if [ ! -e ${FILEDIR}/Ubuntu-Packages-Update.service ]; then
-	cat > ${FILEDIR}/Ubuntu-Packages-Update.service <<EOF
-	[Unit]
-	Description=Updates Ubuntu Packages
-	Wants=Ubuntu-Packages-Update.timer
-
-	[Service]
-	Type=oneshot
-	ExecStart=sudo apt update -y
-
-	[Install]
-	WantedBy=multi-user.target
-EOF
-
-if [ $? == 0 ]; then
-	if [ ! -e ${FILEDIR}/Ubuntu-Packages-Update.timer ]; then
-		 cat > ${FILEDIR}/Ubuntu-Packages-Update.timer <<EOF
-		[Unit]
-		Description=Calls the Ubuntu-Packages-Update.service Weekly on Monday at 2am.
-		Requires=Ubuntu-Packages-Update.service
-
-		[Timer]
-		Unit=Ubuntu-Packages-Update.service
-		OnCalendar=Mon *-*-* 02:00:00 
-
-		[Install]
-		WantedBy=timers.target
-EOF
-
-	fi
+	echo "Creating files Ubuntu-Packages-Update.service and Ubuntu-Packages-Update.timer in /etc/systemd/system"
+	sleep 2
 else
-	echo "There was an issue creating the service file."
+	echo "Files already Exist."
 	exit 1
 fi
+
+cat > ${FILEDIR}/Ubuntu-Packages-Update.service <<EOF
+[Unit]
+Description=Updates Ubuntu Packages
+Wants=Ubuntu-Packages-Update.timer
+
+[Service]
+Type=oneshot
+ExecStart=sudo apt update -y
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > ${FILEDIR}/Ubuntu-Packages-Update.timer <<EOF
+[Unit]
+Description=Calls the Ubuntu-Packages-Update.service Weekly on Monday at 2am.
+Requires=Ubuntu-Packages-Update.service
+
+[Timer]
+Unit=Ubuntu-Packages-Update.service
+OnCalendar=Mon *-*-* 02:00:00 
+
+[Install]
+WantedBy=timers.target
+EOF
 
 if [ $? == 0 ]; then
 	#Modify permissions for root to have Full privileges.
@@ -55,6 +52,8 @@ if [ $? == 0 ]; then
 		echo "File permissions changed for Ubuntu-Packages-Update.service and Ubuntu-Packages-Update.timer"
 		echo "Enabling the timer."
 		systemctl enable Ubuntu-Packages-Update.timer
+		echo "Starting the timer."
+		systemctl start Ubuntu-Packages-Update.timer
 		if [ $? == 0 ]; then
 			echo "Reloading daemon."
 			systemctl daemon-reload
